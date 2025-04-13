d7000ac6fea959f6334e1e2a6f457568ba1df0c3
tharunkumar-BIT <149181225+tharunkumar-BIT@users.noreply.github.com>
2025-04-13 21:25:14 +0530

    search function done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,12 +4,25 @@ import "./App.css";
 
 function App() {
   const [users, setUsers] = useState([]);
+  const [filterUsers, setFilterUsers] = useState([]);
 
   const getUsers = async () => {
     await axios.get("http://localhost:8000/users").then((res) => {
       console.log(res.data);
       setUsers(res.data);
+      setFilterUsers(res.data);
     });
+  };
+
+  //search function
+  const handleSearchChange = (e) => {
+    const searchString = e.target.value.toLowerCase();
+    const filteredUsers = users.filter(
+      (user) =>
+        user.name.toLowerCase().includes(searchString) ||
+        user.city.toLowerCase().includes(searchString)
+    );
+    setFilterUsers(filteredUsers);
   };
 
   useEffect(() => {
@@ -20,7 +33,11 @@ function App() {
       <div className="container">
         <h2>CRUD Application using React and Node</h2>
         <div className="input-search">
-          <input type="search" placeholder="Enter element to Search" />
+          <input
+            type="search"
+            placeholder="Enter element to Search"
+            onChange={handleSearchChange}
+          />
           <button className="btn green">Add Record</button>
         </div>
         <table className="table">
@@ -35,21 +52,21 @@ function App() {
             </tr>
           </thead>
           <tbody>
-            {users &&
-              users.map((user, index) => {
-                return(
+            {filterUsers &&
+              filterUsers.map((user, index) => {
+                return (
                   <tr key={index}>
-                  <td>{user.id}</td>
-                  <td>{user.name}</td>
-                  <td>{user.age}</td>
-                  <td>{user.city}</td>
-                  <td>
-                    <button className="btn green">Edit</button>
-                  </td>
-                  <td>
-                    <button className="btn red">Delete</button>
-                  </td>
-                </tr>
+                    <td>{user.id}</td>
+                    <td>{user.name}</td>
+                    <td>{user.age}</td>
+                    <td>{user.city}</td>
+                    <td>
+                      <button className="btn green">Edit</button>
+                    </td>
+                    <td>
+                      <button className="btn red">Delete</button>
+                    </td>
+                  </tr>
                 );
               })}
           </tbody>
